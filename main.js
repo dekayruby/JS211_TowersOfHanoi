50d229ab8cfd0edf749abf10f6d8d461e1597d7d
dekayruby <ruby.dekay99@gmail.com>
2021-07-16 18:30:45 -0500

    Final changes to the checkpoint

--- main.js
+++ main.js
@@ -22,35 +22,73 @@ let stacks = {
   c: []
 };
 
-// Start here. What is this function doing?
+
+//This function is console logging the stacks
 const printStacks = () => {
   console.log("a: " + stacks.a);
   console.log("b: " + stacks.b);
   console.log("c: " + stacks.c);
 }
 
-// Next, what do you think this function should do?
-const movePiece = () => {
-  // Your code here
 
+const movePiece = (startStack, endStack) => {
+  //This grabs the last number in the starting stack array
+  let start = stacks[startStack].pop()
+  let end = stacks[endStack]
+  //This pushes it to the end of the ending stack array
+  return end.push(start)
 }
 
-// Before you move, should you check if the move it actually allowed? Should 3 be able to be stacked on 2
-const isLegal = () => {
-  // Your code here
 
+const isLegal = (s,e) => {
+let start = stacks[s]
+let end = stacks[e]
+//This says that you can't move from an empty stack
+if (start.length == 0){
+  return false
+  }
+//This says that if the value of startstack is less than the endstack, it can move
+if((start[start.length-1] < end[end.length-1]) || (end.length === 0)) {
+  return true
+  }
+//This will catch any other illegal moves
+else{
+  return false
+  }
 }
 
-// What is a win in Towers of Hanoi? When should this function run?
+
+
+
 const checkForWin = () => {
-  // Your code here
+  //beginStack is how the stack should look, in either spot b or c if there is a win.
+  let beginStack = [4,3,2,1]
+  //I used .every() because it loops over the array and returns a boolean value based on the logic, so checkB and checkC will return true if it matches beginStack
+  let checkB = beginStack.length == stacks.b.length && beginStack.every(function(element, index) {
+    return element === stacks.b[index]; 
+    });
+  let checkC = beginStack.length == stacks.c.length && beginStack.every(function(element, index) {
+    return element === stacks.c[index]; 
+    });
 
+  //This says if either checkB or checkC is true, then return true
+  if( checkB || checkC ) {
+     console.log('You Win!')
+    return true
+  } else {
+    return false
+  }
 }
 
 // When is this function called? What should it do with its argument?
+//This function is called in the getPrompt function, after the user inputs their starting and ending stacks
+//It uses the arguments as the users input for starting stack and ending stack
 const towersOfHanoi = (startStack, endStack) => {
-  // Your code here
-
+//The function checks if the move was legal, moves the piece if so, and then checks if there was a win.
+  if(isLegal(startStack, endStack) ){
+    movePiece(startStack, endStack);
+  }
+   checkForWin();
 }
 
 const getPrompt = () => {
